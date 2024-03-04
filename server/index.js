476c94bb37f14e81acf635cf48dcc168406ea5ae
durveshrv <129104239+durveshrv@users.noreply.github.com>
2024-03-04 19:37:48 +0530

    com

--- server/index.js
+++ server/index.js
@@ -211,7 +211,10 @@ app.get("/api/users", async (req, res) => {
   }
 });
 const port = process.env.PORT || 4000;
-
+app.use(express.static("../frontend/build"));
+app.get("*",(req,res)=>{
+  res.sendFile(path.resolve(__dirname,"frontend","build","index.html"))
+})
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
